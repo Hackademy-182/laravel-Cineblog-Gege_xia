b6ab7438b59168a94291daa88d98703d2196f306
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-19 17:21:15 +0100

    inizio layout nav hero cards footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="py-4 border-top bg-white">
+    <div class="container small text-muted">
+        © {{ date('Y') }} CineHouse
+        <span class="ms-2">Cinehouse@</span>
+    </div>
+</footer>
